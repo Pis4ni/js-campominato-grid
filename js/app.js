8b1c7b7111ecf58bcad74f64492422944e8d2c60
Pis4ni <pis4wrk@gmail.com>
2023-08-21 20:09:04 +0200

    add: bonus logic

--- js/app.js
+++ js/app.js
@@ -1,29 +1,48 @@
 // recuper dal dom gli elementi di interesse
 const cellsContainer = document.getElementById('cells-container')
 const startBtn = document.getElementById('generate-grid')
+const difficulty = document.getElementById('difficulty')
+let difficultyValue = difficulty.value
+console.log(difficultyValue);
 // todo totale celle temporaneo da legare alla select futura
-const cellsTotal = 8*8
+let cellsTotal = difficultyValue * difficultyValue;
+console.log(cellsTotal);
 // ! ON LOAD
 
-generateGrid(cellsTotal, cellsContainer)
+generateGrid(cellsTotal, cellsContainer, difficultyValue)
+console.log(cellsTotal);
 
 
 
 // ! STARTBUTTON CLICK
 startBtn.addEventListener('click', () =>{
     cellsContainer.innerHTML = ''
-
+    difficultyValue = difficulty.value
+    cellsTotal = difficultyValue * difficultyValue
     for (let i = 1; i <= cellsTotal; i++) {
-        generateCell(cellsContainer, i);
+        generateCell(cellsContainer, i, difficultyValue);
     }
+    console.log(cellsTotal);
 })
 
 // ! FUNCTIONS
 
-function generateCell(container, i){
+function generateCell(container, i, difficulty){
     // creo l'elemento cella
     const cell = document.createElement('li')
-    // todo da mettere il valore della cella non in innerHTML
+    if (difficulty == 10) {
+        cell.classList.remove('w-medium')
+        cell.classList.remove('w-hard')
+        cell.classList.add('w-easy')
+    }else if (difficulty == 9) {
+        cell.classList.remove('w-easy')
+        cell.classList.remove('w-hard')
+        cell.classList.add('w-medium') 
+    }else if (difficulty == 7) {
+        cell.classList.remove('w-easy')
+        cell.classList.remove('w-medium')
+        cell.classList.add('w-hard') 
+    }
     cell.setAttribute('data-index', i)
     cell.classList.add('cell')
 
@@ -34,18 +53,18 @@ function generateCell(container, i){
             this.classList.remove('cell-odd') 
             this.classList.remove('cell-even') 
             this.innerText = '';
-        }else if (index % 2 == 0 ) {
-            this.classList.add('cell-odd')
         }else if (index % 2 !== 0 ) {
+            this.classList.add('cell-odd')
+        }else if (index % 2 == 0 ) {
             this.classList.add('cell-even')}
     })
 
     cellsContainer.append(cell)
     
 }
-function generateGrid(cellsNumber, container){
+function generateGrid(cellsNumber, container, difficulty){
     cellsContainer.innerHTML = ''
     for (let i = 1; i <= cellsNumber; i++) {
-        generateCell(container, i);
+        generateCell(container, i, difficulty);
     }
 }
